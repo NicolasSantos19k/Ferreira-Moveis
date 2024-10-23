b40add1423ab1fb9871e1fb532b99330e779a5ea
NicolasSantos19k <144915645+NicolasSantos19k@users.noreply.github.com>
2024-10-23 16:48:56 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,60 @@
+ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.efeito-1-card', {
+    duration: 1500,
+    distance: '80px'
+})
+
+ScrollReveal().reveal('.efeito-2-card', {
+    duration: 1500,
+    distance: '80px',
+    delay: 200
+})
+
+ScrollReveal().reveal('.efeito-3-card', {
+    duration: 1500,
+    distance: '80px',
+    delay: 400
+})
+
+ScrollReveal().reveal('.efeito-4-card', {
+    duration: 1500,
+    distance: '80px',
+    delay: 600
+})
+
+//sofa//
+
+ScrollReveal().reveal('.sofa-animation', {
+    duration: 1500,
+    distance: '80px',
+    origin: 'left',
+})
+ScrollReveal().reveal('.sofa-animation-2', {
+    duration: 1500,
+    distance: '80px',
+    origin: 'top',
+    delay: 200
+})
+
+//cards-2//
+
+ScrollReveal().reveal('.card-2-animation-1', {
+    duration: 1500,
+    distance: '80px',
+    origin: 'bottom',
+})
+
+ScrollReveal().reveal('.card-2-animation-2', {
+    duration: 1500,
+    distance: '80px',
+    origin: 'top',
+    delay: 200
+})
+
+ScrollReveal().reveal('.card-2-animation-3', {
+    duration: 1500,
+    distance: '80px',
+    origin: 'bottom',
+    delay: 400
+})
